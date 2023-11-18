fed0fa16ee51e8ca33332ea5ffd51e023823ca03
Andrea Valenza <andrea.valenza@prima.it>
2023-11-18 17:22:13 +0100

    Support file arguments

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from z3 import *
 from parser import regex_to_z3_expr
 import sre_parse
 
+import os
 import typer
 from typing import List
 from typing_extensions import Annotated
 
 app = typer.Typer()
+
+def fetch_regex(regex, partial=False):
+    # Also accept a file list to regex
+    if os.path.exists(regex):
+        with open(regex, 'r') as regex_file:
+            for regex in regex_file.readlines():
+                regex = regex.strip()
+                regex = f".*{regex}.*" if partial else regex
+                yield regex_to_z3_expr(sre_parse.parse(regex)), regex
+                
+    else: 
+        regex = f".*{regex}.*" if partial else regex
+        yield regex_to_z3_expr(sre_parse.parse(regex)), regex
+
 
 @app.command()
 def generate(
@@ -27,36 +42,35 @@ def generate(
         print("# Full matches")
 
     # Fully Match these regex
-    for regex in matching:
-        if verbose:
-            print(f" [+] {regex}")
-        r = regex_to_z3_expr(sre_parse.parse(regex))
-        solver.add(InRe(bypass, r))
+    for m in matching:
+        for r, regex_string in fetch_regex(m):
+            if verbose:
+                print(f" [+] {regex_string}")
+            solver.add(InRe(bypass, r))
 
     # Don't fully match these regex
     for regex in not_matching:
-        if verbose:
-            print(f" [-] {regex}")
-        r = regex_to_z3_expr(sre_parse.parse(regex))
-        solver.add(InRe(bypass, Complement(r)))
+        for r, regex_string in fetch_regex(regex, partial=True):
+            if verbose:
+                print(f" [-] {regex_string}")
+            solver.add(InRe(bypass, Complement(r)))
 
     if verbose and (partial_matching or not_partial_matching):
         print("## Partial Matches")
 
     # Partially match these regex
     for regex in partial_matching:
-        if verbose:
-            print(f"  +  {regex}")
-        r = regex_to_z3_expr(sre_parse.parse(f".*{regex}.*"))
-        solver.add(InRe(bypass, r))
+        for r, regex_string in fetch_regex(regex, partial=True):
+            if verbose:
+                print(f"  +  {regex_string}")
+            solver.add(InRe(bypass, r))
 
     # Don't partially match these regex
-    if verbose and not_partial_matching:
-        if verbose:
-            print(f"  -  {regex}")
     for regex in not_partial_matching:
-        r = regex_to_z3_expr(sre_parse.parse(f".*{regex}.*"))
-        solver.add(InRe(bypass, Complement(r)))
+        for r, regex_string in fetch_regex(regex, partial=True):
+            if verbose:
+                print(f"  -  {regex_string}")
+            solver.add(InRe(bypass, Complement(r)))
 
     solver.add(Length(bypass) >= min_len)
     solver.add(Length(bypass) <= max_len)
